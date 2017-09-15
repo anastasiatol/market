f767b5ace8740aafcc4f516878b56577584ef385
anastasiatol <tolstykoanastasia@gmail.com>
2017-09-15 10:55:49 +0300

    design main page

--- controller/goodsController.js
+++ controller/goodsController.js
@@ -1,10 +1,11 @@
 angular.
     module('goodsApp').
     controller('goodsController', function ($scope){
-         $scope.$on("openModalInfoGood", function (event, good){
+        $scope.$on("openModalInfoGood", function (event, good){
              $scope.showModalInfo = true;
-             console.log(good);
-             console.log($scope.showModalInfo);
              $scope.selectedGood = good;
-         })       
+        })
+        $scope.onClose = function (){
+            $scope.showModalInfo = false;
+        }
 })
